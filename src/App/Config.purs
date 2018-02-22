d0d8e4ad37f51a8f147ca8b324d0a418981a723c
Irakli Safareli <i.safareli@gmail.com>
2018-02-23 00:23:17 +0400

    make config safer

--- src/App/Config.purs
+++ src/App/Config.purs
@@ -1,11 +1,25 @@
 module Config (config) where
 
 import Prelude
-import Data.Maybe (fromJust)
+
+import Control.Monad.Except (runExcept)
+import Data.Either (hush)
+import Data.Foreign (Foreign, readString)
+import Data.Maybe (Maybe(..))
 import Network.Ethereum.Web3 (Address, mkHexString, mkAddress)
-import Partial.Unsafe (unsafePartial)
+import Partial.Unsafe (unsafeCrashWith)
 
-foreign import simpleStorageAddr :: String
+foreign import simpleStorageAddr :: Foreign
 
-config :: {simpleStorageAddress :: Address }
-config = {simpleStorageAddress: unsafePartial fromJust $ mkAddress =<< mkHexString simpleStorageAddr}
+config :: { simpleStorageAddress :: Address }
+config = { simpleStorageAddress }
+  where
+  simpleStorageAddress = 
+    let
+      addrMb = do
+        str <- hush $ runExcept $ readString simpleStorageAddr
+        hex <- mkHexString str
+        mkAddress hex
+    in case addrMb of
+      Just a -> a
+      Nothing -> unsafeCrashWith "Make sure environment variable `SIMPLE_STORAGE_ADDRESS` was set to correct hex address during build"
