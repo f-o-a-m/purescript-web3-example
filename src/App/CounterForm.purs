aece0b9d4ca1f1112377dea617626bfb94702825
Irakli Safareli <i.safareli@gmail.com>
2018-03-03 16:29:08 +0400

    fallback to pull aproach when `event` fails

--- src/App/CounterForm.purs
+++ src/App/CounterForm.purs
@@ -5,7 +5,7 @@ import Prelude
 import App.Uport (uportProvider')
 import Contracts.SimpleStorage as SimpleStorage
 import Control.Error.Util (note)
-import Control.Monad.Aff (Aff, launchAff, liftEff', Milliseconds(..), delay)
+import Control.Monad.Aff (Aff, Error, Milliseconds(..), delay, forkAff, joinFiber, launchAff, liftEff', try)
 import Control.Monad.Eff.Class (liftEff)
 import Control.Monad.Trans.Class (lift)
 import Data.Array (index)
@@ -18,8 +18,9 @@ import Data.Tuple (Tuple(..))
 import MaterialUI (EventHandlerOpt(..), UnknownType(..), stringNode)
 import MaterialUI.RaisedButton as RaisedButton
 import MaterialUI.TextField as TextField
-import Network.Ethereum.Web3 (ChainCursor(..), ETH, EventAction(..), _to, defaultTransactionOptions, event, eventFilter, forkWeb3, runWeb3, Address, _from, decimal, mkAddress, mkHexString, parseBigNumber, uIntNFromBigNumber)
+import Network.Ethereum.Web3 (type (:&), Address, ChainCursor(..), D2, D5, D6, ETH, EventAction(..), Provider, UIntN, Web3Error, _from, _to, decimal, defaultTransactionOptions, event, eventFilter, forkWeb3, mkAddress, mkHexString, parseBigNumber, runWeb3, uIntNFromBigNumber)
 import Network.Ethereum.Web3.Api (eth_getAccounts)
+import Partial.Unsafe (unsafeCrashWith)
 import React as R
 import React.DOM as D
 import React.DOM.Props as P
@@ -35,7 +36,7 @@ type CountFormState =
   { userAddress :: String
   , count :: String
   , errorMessage :: String
-  , currentCount :: String
+  , currentCount :: Maybe (UIntN (D2 :& D5 :& D6))
   , status :: String
   }
 
@@ -44,7 +45,7 @@ initialCountFormState =
     { userAddress: ""
     , count: ""
     , errorMessage: ""
-    , currentCount: ""
+    , currentCount: Nothing
     , status: "Please enter a count."
     }
 
@@ -68,7 +69,7 @@ countFormSpec = T.simpleSpec performAction render
       [ D.div [P.className "status-bar"] [D.text state.status]
       , D.div' 
           [ D.h2 [P.className "count-container"] [ D.text $ "Contract address: " <> show props.contractAddress]
-          , D.h2 [P.className "count-container"] [ D.text $ "Current Count: " <> state.currentCount ]
+          , D.h2 [P.className "count-container"] [ D.text $ "Current Count: " <> maybe "loading" show state.currentCount ]
           ]
       , D.div [P.className ""]
          [ D.h3 [P.className "error-message-container"]
@@ -131,34 +132,90 @@ countFormClass =
     in R.createClass (spec { componentWillMount = componentWillMount })
   where
   componentWillMount :: forall eff. R.ComponentWillMount CountFormProps CountFormState (eth :: ETH | eff)
-  componentWillMount this = do 
-    completeAddressField this
-    getInitialState this
-    monitorCount this
-  
-  completeAddressField :: forall eff. R.ComponentWillMount CountFormProps CountFormState ( eth :: ETH | eff)
-  completeAddressField this = void $ launchAff do
-    maddr <- getUserAddress
-    liftEff $ maybe (pure unit) (\a -> void $ R.transformState this _{userAddress = show a}) maddr
-  
-  getInitialState :: forall eff. R.ComponentWillMount CountFormProps CountFormState (eth :: ETH | eff)
-  getInitialState this = void $ launchAff $ do
-    props <- liftEff $ R.getProps this
-    let txOpts = defaultTransactionOptions # _to .~ Just props.contractAddress
+  componentWillMount this = void $ launchAff $ do
     p <- liftEff' uportProvider'
-    c <- runWeb3 p $ SimpleStorage.count txOpts Latest
-    liftEff $ R.transformState this _{currentCount= show c}
+    -- completeAddressField this
+    void $ forkAff $ do
+      readCountAndUpdate this p
+      monitorCount this p
   
-  monitorCount :: forall eff. R.ComponentWillMount CountFormProps CountFormState (eth :: ETH | eff)
-  monitorCount this = void $ launchAff $ do
-    props <- liftEff $ R.getProps this
-    delay (Milliseconds 1000.0)
-    p <- liftEff' uportProvider'
-    void $ forkWeb3 p $ do
-      let fltr = eventFilter (Proxy :: Proxy SimpleStorage.CountSet) props.contractAddress
-      event fltr $ \(SimpleStorage.CountSet cs) -> do
-        _ <- liftEff $ R.transformState this _{currentCount = show cs._count, status = "Transaction succeded, enter new count."}
-        pure ContinueEvent
+  -- completeAddressField :: forall eff. R.ComponentWillMount CountFormProps CountFormState ( eth :: ETH | eff)
+  -- completeAddressField this = void $ launchAff do
+  --   maddr <- getUserAddress
+  --   liftEff $ maybe (pure unit) (\a -> void $ R.transformState this _{userAddress = show a}) maddr
+
+type PropsAndStateRW r =
+  ( props :: R.ReactProps
+  , state :: R.ReactState R.ReadWrite
+  | r)
+
+monitorCount
+  :: forall eff
+   . R.ReactThis CountFormProps CountFormState
+  -> Provider
+  -> Aff (PropsAndStateRW ( eth :: ETH | eff)) Unit
+monitorCount this p = do
+  err <- countEventFilter this p
+  -- traceAnyA err
+  -- NOTE: `err` is ignored as if we will try to read count with pull
+  --  aproach here, which has error handling
+  countPullLoop this p
+
+readCountAndUpdate
+  :: forall eff
+   . R.ReactThis CountFormProps CountFormState
+  -> Provider
+  -> Aff (PropsAndStateRW ( eth :: ETH | eff)) Unit
+readCountAndUpdate this p = do
+  props <- liftEff $ R.getProps this
+  let txOpts = defaultTransactionOptions # _to .~ Just props.contractAddress
+  c <- runWeb3 p $ SimpleStorage.count txOpts Latest
+  case c of
+    Left err -> -- Web3Error
+      liftEff $ R.transformState this _{errorMessage = show err}
+    Right (Left err) -> -- CallError
+      liftEff $ R.transformState this _{errorMessage = show err}
+    Right (Right count) ->
+      liftEff $ R.transformState this _{currentCount = Just count}
+
+countPullLoop
+  :: forall eff void
+   . R.ReactThis CountFormProps CountFormState
+  -> Provider
+  -> Aff (PropsAndStateRW ( eth :: ETH | eff)) void
+countPullLoop this p = do
+  readCountAndUpdate this p
+  delay (Milliseconds 1000.0)
+  countPullLoop this p
+
+countEventFilter
+  :: forall eff
+   . R.ReactThis CountFormProps CountFormState
+  -> Provider
+  -> Aff (PropsAndStateRW ( eth :: ETH | eff)) (Either Error Web3Error)
+countEventFilter this p = do
+  props <- liftEff $ R.getProps this
+  fiber <- forkWeb3 p $ do
+    let fltr = eventFilter (Proxy :: Proxy SimpleStorage.CountSet) props.contractAddress
+    event fltr $ \(SimpleStorage.CountSet cs) -> do
+      _ <- liftEff $ R.transformState this _{currentCount = Just cs._count, status = "Transaction succeded, enter new count."}
+      -- TODO: ideally we should termiante when component is unmounted
+      pure ContinueEvent
+  res <- try (joinFiber fiber)
+  case res of
+    Left err -> do
+      -- # Error from aff
+      -- there is an issue in ps-web3 when sendAsync is called it might fail but that's not handled
+      -- so it propagates up to this point. ideally it should not happen and should be decoded into 
+      -- Web3Error so `try` is not used to catch errors from `sendAsync`
+      pure $ Left err
+    Right (Left err) ->
+      -- # Web3Error
+      -- here are properly handled Web3Errors
+      pure $ Right err
+    Right (Right _) ->
+      -- TODO: ideally we should stop listening when component is unmounted so this shuold be removed
+      unsafeCrashWith "imposible, filter should never finishe, as event handler returns `ContinueEvent` all the time"
 
 --------------------------------------------------------------------------------
 
