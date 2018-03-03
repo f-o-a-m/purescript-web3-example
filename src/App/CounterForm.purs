7522e77c93a4c60100e4380c2467a8e8942b6426
Irakli Safareli <i.safareli@gmail.com>
2018-03-03 20:26:57 +0400

    refactor `status` and take provider as prop

--- src/App/CounterForm.purs
+++ src/App/CounterForm.purs
@@ -2,14 +2,13 @@ module App.CountForm where
 
 import Prelude
 
-import App.Uport (uportProvider')
 import Contracts.SimpleStorage as SimpleStorage
 import Control.Error.Util (note)
-import Control.Monad.Aff (Aff, Error, Milliseconds(..), delay, forkAff, joinFiber, launchAff, liftEff', try)
+import Control.Monad.Aff (Aff, Error, Milliseconds(..), delay, forkAff, joinFiber, launchAff, try)
 import Control.Monad.Eff.Class (liftEff)
 import Control.Monad.Trans.Class (lift)
 import Data.Array (index)
-import Data.Either (Either(..), hush, isLeft)
+import Data.Either (Either(..), hush)
 import Data.Foldable (for_)
 import Data.Foreign (toForeign)
 import Data.Lens ((.~))
@@ -17,13 +16,13 @@ import Data.Maybe (Maybe(..))
 import Data.Monoid (mempty)
 import Data.Options ((:=))
 import Data.Options as Options
-import Data.Tuple (Tuple(..))
 import MaterialUI (EventHandlerOpt(..), UnknownType(..), stringNode)
 import MaterialUI.RaisedButton as RaisedButton
 import MaterialUI.TextField (TextFieldOption)
 import MaterialUI.TextField as TextField
 import Network.Ethereum.Web3 (type (:&), Address, CallError, ChainCursor(..), D2, D5, D6, ETH, EventAction(..), Provider, UIntN, Web3Error, _from, _to, decimal, defaultTransactionOptions, event, eventFilter, forkWeb3, mkAddress, mkHexString, parseBigNumber, runWeb3, uIntNFromBigNumber)
 import Network.Ethereum.Web3.Api (eth_getAccounts)
+import Network.Ethereum.Web3.Types.Types (HexString)
 import Partial.Unsafe (unsafeCrashWith)
 import React as R
 import React.DOM as D
@@ -41,8 +40,13 @@ type CountFormState =
   { userAddress :: InputVal Address
   , count :: InputVal Count
   , currentCount :: Maybe (Either CountFetchError Count)
-  , status :: String
+  , status :: Maybe SubmitStatus
   }
+
+data SubmitStatus
+  = FailedToCreateTransaction Web3Error
+  | TransactionWasCreated HexString
+  | NewValueHasArrived
 
 data CountFetchError
   = FailedToFetchCountWithWeb3Error Web3Error
@@ -55,22 +59,26 @@ emptyInputVal = {val: Left "", input: ""}
 
 initialCountFormState :: CountFormState
 initialCountFormState =
-    { userAddress: emptyInputVal
-    , count: emptyInputVal
-    , currentCount: Nothing
-    , status: "Please enter a count."
-    }
+  { userAddress: emptyInputVal
+  , count: emptyInputVal
+  , currentCount: Nothing
+  , status: Nothing
+  }
 
-data CountFormAction =
-    UpdateAddress String
+data CountFormAction
+  = UpdateAddress String
   | UpdateCount String
   | Submit
 
+type ExtendedProvider =
+  { provider :: Provider
+  , mustSetSender :: Boolean
+  }
 
 type CountFormProps =
   { contractAddress :: Address
+  , provider :: ExtendedProvider
   }
-
 
 strVal :: String -> UnknownType
 strVal = toForeign >>> UnknownType
@@ -90,68 +98,94 @@ listenToInput :: forall action. (action -> T.EventHandler) -> (String -> action)
 listenToInput dispatch ctr =
   TextField.onChange := (EventHandlerOpt $ R.handle $ \e -> dispatch $ ctr (unsafeCoerce e).target.value)
 
+submitDisabled :: CountFormState -> CountFormProps -> Boolean
+submitDisabled st props = case st.count.val of
+  Left _ -> true
+  Right _ | not props.provider.mustSetSender -> false
+  Right _ | otherwise -> case st.userAddress.val of
+    Right _ -> false
+    Left _ -> true
+
 countFormSpec :: forall eff . T.Spec (eth :: ETH | eff) CountFormState CountFormProps CountFormAction
 countFormSpec = T.simpleSpec performAction render
   where
     render :: T.Render CountFormState CountFormProps CountFormAction
     render dispatch props state _ =
-      [ D.div [P.className "status-bar"] [D.text state.status]
-      , D.div' 
-          [ D.h2 [P.className "count-container"]
-              [ D.text $ "Contract address: " <> show props.contractAddress]
-          , D.h2 [P.className "count-container"]
-              [ D.text $ "Current Count: " <> case state.currentCount of 
-                  Nothing -> "loading ..."
-                  Just (Left err) -> "error occurred while fetching count, retrying ..."
-                  Just (Right c) -> show c
+      [ D.h2
+          [ P.className "count-container" ]
+          [ D.text $ "Contract address: " <> show props.contractAddress]
+      , D.h2
+          [ P.className "count-container" ]
+          [ D.text $ "Current Count: " <> case state.currentCount of 
+              Nothing -> "loading ..."
+              Just (Left err) -> "error occurred while fetching count, retrying ..."
+              Just (Right c) -> show c
+          ]
+      , D.form
+          [ P.className "count-form"]
+          [ D.div'
+              [ if props.provider.mustSetSender then
+                  TextField.textField
+                    (listenToInput dispatch UpdateAddress
+                      <> TextField.hintText := stringNode "0xdeadbeef"
+                      <> TextField.fullWidth := true
+                      <> TextField.floatingLabelText := stringNode "User Address *"
+                      <> inputValToOpts state.userAddress
+                    ) []
+                 else
+                   D.text "Sender of the transaction will be set by the provider"
+              ]
+          , D.div'
+              [ TextField.textField
+                  ( listenToInput dispatch UpdateCount
+                    <> TextField.hintText := stringNode "123"
+                    <> TextField.fullWidth := true
+                    <> TextField.floatingLabelText := stringNode "New Count *"
+                    <> inputValToOpts state.count
+                  ) []
+              ]
+          , D.div
+              [ P.className "submit-button-container" ]
+              [ RaisedButton.raisedButton
+                  ( RaisedButton.onClick := (EventHandlerOpt $ R.handle $ \_ -> dispatch Submit)
+                    <> RaisedButton.backgroundColor := "#2196F3"
+                    <> RaisedButton.fullWidth := true
+                    <> RaisedButton.disabled := submitDisabled state props
+                  ) [ D.div
+                      [ P.className "submit-button-text" ]
+                      [ D.text "Submit" ]
+                    ]
               ]
           ]
-      , D.div [P.className ""]
-         [ D.form [P.className "count-form"]
-           [ D.div'
-             [ TextField.textField (listenToInput dispatch UpdateAddress
-                                 <> TextField.hintText := stringNode "0xdeadbeef"
-                                 <> TextField.fullWidth := true
-                                 <> TextField.floatingLabelText := stringNode "User Address *"
-                                 <> inputValToOpts state.userAddress
-                                 ) []
-             ]
-           , D.div'
-             [ TextField.textField (listenToInput dispatch UpdateCount
-                                 <> TextField.hintText := stringNode "123"
-                                 <> TextField.fullWidth := true
-                                 <> TextField.floatingLabelText := stringNode "New Count *"
-                                 <> inputValToOpts state.count
-                                 ) []
-             ]
-            , D.div [P.className "submit-button-container"]
-              [ RaisedButton.raisedButton ( RaisedButton.onClick := (EventHandlerOpt $ R.handle $ \_ -> dispatch Submit)
-                                          <> RaisedButton.backgroundColor := "#2196F3"
-                                          <> RaisedButton.fullWidth := true
-                                          <> RaisedButton.disabled := (isLeft $ Tuple <$> state.userAddress.val <*> state.count.val)
-                                        ) [ D.div
-                                            [ P.className "submit-button-text" ]
-                                            [ D.text "Submit" ]
-                                          ]
-              ]
-            ]
-         ]
+      , D.div [P.className "status-bar"]
+        [ D.text case state.status of
+            Nothing -> "Please enter a count."
+            Just (FailedToCreateTransaction err) -> "Failed to create transaction, try again" -- TODO show err
+            Just (TransactionWasCreated txHash) -> "Transaction was created with Hash: " <> show txHash
+            Just NewValueHasArrived -> "Transaction succeded, enter new count."
+        ]
       ]
 
     performAction :: T.PerformAction (eth :: ETH | eff) CountFormState CountFormProps CountFormAction
     performAction Submit props st = do
-      -- let args = do
-      --       addr <- 
-      --       count <- 
-      --       pure $ Tuple addr count
-      for_ (Tuple <$> st.userAddress.val <*> st.count.val) \(Tuple sender count) -> do
-        p <- lift $ liftEff' uportProvider'
-        txHash <- lift $ runWeb3 p $ do
-          let txOpts = defaultTransactionOptions # _from .~ Just sender
-                                                  # _to .~ Just props.contractAddress
+      for_ st.count.val \count -> do
+        let
+          txOpts = defaultTransactionOptions
+            # _to .~ Just props.contractAddress
+            # case st.userAddress.val of
+              Right sender | props.provider.mustSetSender -> _from .~ Just sender
+              _ ->
+                -- NOTE: submit is disabled if sender is needed,
+                -- so it's safe to ignore other cases in this case match.
+                id
+        mbTxHash <- lift $ runWeb3 props.provider.provider $ do
           SimpleStorage.setCount txOpts { _count : count }
-        pure unit
-        T.modifyState _{ count = emptyInputVal :: InputVal Count, status = "Transaction Hash: " <> show txHash}
+        T.modifyState _
+          { count = emptyInputVal :: InputVal Count
+          , status = Just case mbTxHash of
+              Left err -> FailedToCreateTransaction err 
+              Right txHash -> TransactionWasCreated txHash
+          }
 
     performAction (UpdateAddress input) _ _ = do
       let
@@ -174,21 +208,28 @@ countFormClass =
   where
   componentWillMount :: forall eff. R.ComponentWillMount CountFormProps CountFormState (eth :: ETH | eff)
   componentWillMount this = void $ launchAff $ do
-    p <- liftEff' uportProvider'
-    -- completeAddressField this
+    props <- liftEff $ R.getProps this
+    let p = props.provider.provider
+    when props.provider.mustSetSender
+      $ void $ forkAff $ completeAddressField this p
     void $ forkAff $ do
       readCountAndUpdate this p
       monitorCount this p
-  
-  -- completeAddressField :: forall eff. R.ComponentWillMount CountFormProps CountFormState ( eth :: ETH | eff)
-  -- completeAddressField this = void $ launchAff do
-  --   maddr <- getUserAddress
-  --   liftEff $ maybe (pure unit) (\a -> void $ R.transformState this _{userAddress = show a}) maddr
 
 type PropsAndStateRW r =
   ( props :: R.ReactProps
   , state :: R.ReactState R.ReadWrite
   | r)
+
+completeAddressField
+  :: forall eff
+  . R.ReactThis CountFormProps CountFormState
+  -> Provider
+  -> Aff (PropsAndStateRW ( eth :: ETH | eff)) Unit
+completeAddressField this p = do
+  accounts <- map hush $ runWeb3 p $ eth_getAccounts
+  for_ (accounts >>= flip index 0) \addr -> do
+    liftEff $ R.transformState this _{userAddress = {val: Right addr, input: show addr}}
 
 monitorCount
   :: forall eff
@@ -217,7 +258,15 @@ readCountAndUpdate this p = do
     Right (Left err) ->
       liftEff $ R.transformState this _{currentCount = Just $ Left $ FailedToFetchCountWithCallError err}
     Right (Right count) ->
-      liftEff $ R.transformState this _{currentCount = Just $ Right count}
+      liftEff $ R.transformState this \s -> 
+        -- NOTE: besides updating `currentCount` here we also try to detect if the value
+        -- is new, so `status` is updated in same way as it would in `countEventFilter`
+        let nextCount = Just $ Right count
+        in case s.status of
+          Just (TransactionWasCreated _) -> case s.currentCount of
+            Just (Right count') | count == count' -> s
+            _ -> s{currentCount = nextCount, status = Just NewValueHasArrived}
+          _ -> s{currentCount = nextCount}
 
 countPullLoop
   :: forall eff void
@@ -239,7 +288,7 @@ countEventFilter this p = do
   fiber <- forkWeb3 p $ do
     let fltr = eventFilter (Proxy :: Proxy SimpleStorage.CountSet) props.contractAddress
     event fltr $ \(SimpleStorage.CountSet cs) -> do
-      _ <- liftEff $ R.transformState this _{currentCount = Just $ Right cs._count, status = "Transaction succeded, enter new count."}
+      _ <- liftEff $ R.transformState this _{currentCount = Just $ Right cs._count, status = Just NewValueHasArrived}
       -- TODO: ideally we should termiante when component is unmounted
       pure ContinueEvent
   res <- try (joinFiber fiber)
@@ -257,11 +306,3 @@ countEventFilter this p = do
     Right (Right _) ->
       -- TODO: ideally we should stop listening when component is unmounted so this shuold be removed
       unsafeCrashWith "imposible, filter should never finishe, as event handler returns `ContinueEvent` all the time"
-
---------------------------------------------------------------------------------
-
-getUserAddress :: forall eff . Aff (eth :: ETH | eff) (Maybe Address)
-getUserAddress = do
-  p <- liftEff' uportProvider'
-  accounts <- map hush $ runWeb3 p $ eth_getAccounts
-  pure $ accounts >>= flip index 0
--- src/Main.purs
+++ src/Main.purs
@@ -2,53 +2,69 @@ module Main where
 
 import Prelude
 
-import App.CountForm (countFormClass)
+import App.CountForm (CountFormProps, countFormClass, ExtendedProvider)
 import App.MaterialUI (muiThemeProviderClass)
 import Control.Monad.Eff (Eff)
+import Control.Monad.Except (runExcept)
 import DOM (DOM)
 import DOM.HTML (window)
 import DOM.HTML.Types (htmlDocumentToDocument)
 import DOM.HTML.Window (document)
 import DOM.Node.NonElementParentNode (getElementById)
 import DOM.Node.Types (Element, ElementId(..), documentToNonElementParentNode)
-import Data.Foldable (for_)
-import React as R
-import ReactDOM (render)
-import Control.Monad.Except (runExcept)
 import Data.Either (hush)
+import Data.Foldable (for_)
 import Data.Foreign (Foreign, readString)
 import Data.Maybe (Maybe(..))
-import Network.Ethereum.Web3 (Address, mkHexString, mkAddress)
+import Network.Ethereum.Web3 (Address, metamaskProvider, mkAddress, mkHexString)
 import Partial.Unsafe (unsafeCrashWith)
+import React as R
+import ReactDOM (render)
+import Network.Ethereum.Uport (AppName(..), UPORT, connect, rinkeby, withAppName, withNetwork, getProvider)
+import Control.Monad.Eff.Exception (EXCEPTION)
 
-
-
-main :: forall eff. Eff (dom :: DOM | eff) Unit
+main :: forall eff. Eff (dom :: DOM, exception :: EXCEPTION, uport :: UPORT | eff) Unit
 main = do
   elem <- getElem
-  for_ elem (render ui)
+  provider <- mkUport
+  -- provider <- mkMetamask
+  let props = { contractAddress, provider }
+  for_ elem $ render $ ui props
   where
-    ui :: R.ReactElement
-    ui =
+    ui :: CountFormProps -> R.ReactElement
+    ui props =
       R.createElement muiThemeProviderClass unit
         [ R.createFactory countFormClass props ]
-    getElem :: Eff (dom :: DOM | eff) (Maybe Element)
-    getElem = do
-      win <- window
-      doc <- document win
-      getElementById (ElementId "app") (documentToNonElementParentNode (htmlDocumentToDocument doc))
 
-props :: { contractAddress :: Address }
-props = { contractAddress }
-  where
-  contractAddress = 
-    let
-      addrMb = do
-        str <- hush $ runExcept $ readString simpleStorageAddr
-        hex <- mkHexString str
-        mkAddress hex
-    in case addrMb of
-      Just a -> a
-      Nothing -> unsafeCrashWith "Make sure environment variable `SIMPLE_STORAGE_ADDRESS` was set to correct hex address during build"
+getElem :: forall eff. Eff (dom :: DOM | eff) (Maybe Element)
+getElem = do
+  win <- window
+  doc <- document win
+  getElementById (ElementId "app") (documentToNonElementParentNode (htmlDocumentToDocument doc))
+
+mkUport :: forall eff. Eff (exception :: EXCEPTION, uport :: UPORT | eff) ExtendedProvider
+mkUport = do
+  let
+    uportConnect = connect
+      $ withAppName (AppName "Web3 example")
+      >>> withNetwork rinkeby
+  provider <- uportConnect >>= getProvider
+  pure { provider, mustSetSender: false }
+
+mkMetamask :: forall eff. Eff (exception :: EXCEPTION | eff) ExtendedProvider
+mkMetamask = do
+  provider <- metamaskProvider
+  pure {provider, mustSetSender: true}
+
+contractAddress :: Address
+contractAddress =
+  let
+    addrMb = do
+      str <- hush $ runExcept $ readString simpleStorageAddr
+      hex <- mkHexString str
+      mkAddress hex
+  in case addrMb of
+    Just a -> a
+    Nothing -> unsafeCrashWith "Make sure environment variable `SIMPLE_STORAGE_ADDRESS` was set to correct hex address during build"
 
 foreign import simpleStorageAddr :: Foreign
